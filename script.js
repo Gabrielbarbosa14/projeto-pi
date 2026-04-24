342ff7308dd2d49e52adcc6a9ccaba1aede73e21
Gabrielbarbosa14 <terezamariabarbosa635@gmail.com>
2026-04-24 09:48:17 -0300

    Testando 2

---
+++ script.js
@@ -0,0 +1,12 @@
+/*const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("mouseenter", () => {
+    card.style.transform = "translateY(-20px) scale(1.15)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "translateY(0) scale(1)";
+  });
+});
+*/
